12b4a805c7a0e68df5e81db74493aa0a5c97b759
Dmytro <pelexov.mitya@gmail.com>
2024-02-20 18:27:00 +0200

    реалізація виведення title вибраного товару у кошик через перетворення productsArray в productsObject

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,17 +1,26 @@
-import { productsArray } from 'utils/productsArray'
+import { Product, getProductsObject, productsArray } from 'utils/productsArray'
 
 type CartHeaderType = {
     productsInCart: { [id: number]: number }
+    productsObject?: { [id: number]: Product }
 }
 
-const CartHeader = ({ productsInCart }: CartHeaderType) => {
+const CartHeader = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: CartHeaderType) => {
+    console.log(productsObject)
+    // бачимо,що ключами  об'єкту є id, а значеннями- об'єкти product
+    console.log(productsInCart)
+    // бачимо, що ключами об'єкту є id, а значеннями- кількість вибраного товару
     return (
         <div>
             {Object.keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productsArray[+productId - 1].title}:
+                    {productsObject[+productId].title}:
                     {productsInCart[+productId]}
                 </div>
+                // ключі об'єкту- title вибраного товару, а значення- його кількість
             ))}
         </div>
     )
--- src/utils/productsArray.ts
+++ src/utils/productsArray.ts
@@ -1,62 +1,75 @@
-export type Product={
-    id:number
+export type Product = {
+    id: number
     title: string
     description: string
     type?: string
     capacity: number
     price: number
-    image:string
+    image: string
 }
 // ми описали тип одного продукту
 
+export const productsArray: Product[] = [
+    {
+        // тут ми показуємо, що productsArray це масив з Product
+        id: 1,
+        title: 'iPhone 15 Pro',
+        description: 'This is iPhone 15 Pro',
+        type: 'phone',
+        capacity: 128,
+        price: 1000,
+        image: '/images/iphone-black.webp',
+    },
+    {
+        id: 2,
+        title: 'iPhone 14 Pro',
+        description: 'This is iPhone 14 Pro',
+        type: 'phone',
+        capacity: 28,
+        price: 2000,
+        image: '/images/iphone-gold.webp',
+    },
+    {
+        id: 3,
+        title: 'iPhone 13 Pro',
+        description: 'This is iPhone 13 Pro',
+        type: 'phone',
+        capacity: 8,
+        price: 3000,
+        image: '/images/iphone-blue.webp',
+    },
+    {
+        id: 4,
+        title: 'iPhone 12 Pro',
+        description: 'This is iPhone 12 Pro',
+        type: 'phone',
+        capacity: 82,
+        price: 4000,
+        image: '/images/iphone-green.webp',
+    },
+    {
+        id: 5,
+        title: 'iPhone 11 Pro',
+        description: 'This is iPhone 11 Pro',
+        type: 'phone',
+        capacity: 821,
+        price: 5000,
+        image: '/images/iphone-pink.webp',
+    },
+    {
+        id: 6,
+        title: 'iPhone 10 Pro',
+        description: 'This is iPhone 10 Pro',
+        type: 'phone',
+        capacity: 88,
+        price: 6000,
+        image: '/images/iphone-red.webp',
+    },
+]
 
-export const productsArray:Product[]=[{
-    // тут ми показуємо, що productsArray це масив з Product
-    id:1,
-    title: 'iPhone 15 Pro',
-    description: 'This is iPhone 15 Pro',
-    type: 'phone',
-    capacity: 128,
-    price: 1000,
-    image:'/images/iphone-black.webp'
-},{
-    id:2,
-    title: 'iPhone 14 Pro',
-    description: 'This is iPhone 14 Pro',
-    type: 'phone',
-    capacity: 28,
-    price: 2000,
-    image:'/images/iphone-gold.webp'
-},{
-    id:3,
-    title: 'iPhone 13 Pro',
-    description: 'This is iPhone 13 Pro',
-    type: 'phone',
-    capacity: 8,
-    price: 3000,
-    image:'/images/iphone-blue.webp'
-},{
-    id:4,
-    title: 'iPhone 12 Pro',
-    description: 'This is iPhone 12 Pro',
-    type: 'phone',
-    capacity: 82,
-    price: 4000,
-    image:'/images/iphone-green.webp'
-},{
-    id:5,
-    title: 'iPhone 11 Pro',
-    description: 'This is iPhone 11 Pro',
-    type: 'phone',
-    capacity: 821,
-    price: 5000,
-    image:'/images/iphone-pink.webp'
-},{
-    id:6,
-    title: 'iPhone 10 Pro',
-    description: 'This is iPhone 10 Pro',
-    type: 'phone',
-    capacity: 88,
-    price: 6000,
-    image:'/images/iphone-red.webp'
-}]
+// для того,щоб нам в кошику коректно відображалася назва вибраного товару, нам необхідно перетворити масив в об'єкт
+export const getProductsObject = (array: Product[]) =>
+    array.reduce(
+        (object, product) => ({ ...object, [product.id]: product }),
+        {}
+    )
