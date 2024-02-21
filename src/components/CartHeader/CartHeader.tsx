fe71f648462a073a94396a0d1bdd30e456ebe70d
Dmytro <pelexov.mitya@gmail.com>
2024-02-21 19:54:14 +0200

    створили перехід між сторінками, створили перевикористовуємі компоненти CartProductList i CartTotal

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,41 +1,20 @@
-import { Product, getProductsObject, productsArray } from 'utils/productsArray'
+import CartProductList from 'components/CartProductList/CartProductList'
+import CartTotal from 'components/CartTotal/CartTotal'
 
 type CartHeaderType = {
     productsInCart: { [id: number]: number }
-    productsObject?: { [id: number]: Product }
 }
 
-const CartHeader = ({
-    productsInCart,
-    productsObject = getProductsObject(productsArray),
-}: CartHeaderType) => {
-    console.log(productsObject)
+const CartHeader = ({ productsInCart }: CartHeaderType) => {
+    // console.log(productsObject)
     // бачимо,що ключами  об'єкту є id, а значеннями- об'єкти product
-    console.log(productsInCart)
+    // console.log(productsInCart)
     // бачимо, що ключами об'єкту є id, а значеннями- кількість вибраного товару
 
     return (
         <div>
-            <div>
-                {Object.keys(productsInCart).map((productId) => (
-                    <div key={productId}>
-                        {productsObject[+productId].title}:
-                        {productsInCart[+productId]}
-                    </div>
-                    // ключі об'єкту- title вибраного товару, а значення- його кількість
-                ))}
-            </div>
-            <div>
-                Total:
-                {Object.keys(productsInCart).reduce(
-                    (sum, productId) =>
-                        sum +
-                        productsObject[+productId].price *
-                            productsInCart[+productId],
-                    0
-                )}
-                $
-            </div>
+            <CartProductList productsInCart={productsInCart} />
+            <CartTotal productsInCart={productsInCart} />
         </div>
     )
 }
--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
-import Main from './Main'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
+import { Route, Routes } from 'react-router'
+import { Container } from '@mui/material'
+import Home from 'pages/Home/Home'
+import CartPage from 'pages/CartPage/CartPage'
 
 type Props = {}
 
@@ -46,7 +49,19 @@ const App = (props: Props) => {
             {/* <button onClick={() => addProductToCart(2, 5)}>
                 Add to cart(id:2,count:5)
             </button> */}
-            <Main addProductToCart={addProductToCart} />
+            <Container maxWidth="lg" sx={{ padding: '50px 0px' }}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route
+                        path="cart"
+                        element={<CartPage productsInCart={productsInCart} />}
+                    />
+                </Routes>
+            </Container>
+
             <Footer />
         </StyledEngineProvider>
     )
