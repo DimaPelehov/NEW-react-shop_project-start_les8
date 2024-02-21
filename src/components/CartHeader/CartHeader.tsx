dbb3d7a2fdef0e4804299674c5dc7bfe163d4204
Dmytro <pelexov.mitya@gmail.com>
2024-02-21 15:18:41 +0200

    додали підрахунок загальної вартості вибраних товарів

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -13,15 +13,29 @@ const CartHeader = ({
     // бачимо,що ключами  об'єкту є id, а значеннями- об'єкти product
     console.log(productsInCart)
     // бачимо, що ключами об'єкту є id, а значеннями- кількість вибраного товару
+
     return (
         <div>
-            {Object.keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productsObject[+productId].title}:
-                    {productsInCart[+productId]}
-                </div>
-                // ключі об'єкту- title вибраного товару, а значення- його кількість
-            ))}
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[+productId].title}:
+                        {productsInCart[+productId]}
+                    </div>
+                    // ключі об'єкту- title вибраного товару, а значення- його кількість
+                ))}
+            </div>
+            <div>
+                Total:
+                {Object.keys(productsInCart).reduce(
+                    (sum, productId) =>
+                        sum +
+                        productsObject[+productId].price *
+                            productsInCart[+productId],
+                    0
+                )}
+                $
+            </div>
         </div>
     )
 }
