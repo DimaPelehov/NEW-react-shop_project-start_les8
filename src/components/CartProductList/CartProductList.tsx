fda8cc4dbb4e5577ee3d305b2f9140efc5b1ecd2
Dmytro <pelexov.mitya@gmail.com>
2024-03-14 19:20:43 +0200

    зробили CartProductListItem(звичайну корзину) та CartProductListItemExtended(розширену)

--- src/components/CartProductList/CartProductList.tsx
+++ src/components/CartProductList/CartProductList.tsx
@@ -1,28 +1,27 @@
 import { Product, getProductsObject, productsArray } from 'utils/productsArray'
+import CartProductListItem from './CartProductListItem'
 
 type Props = {
     productsInCart: { [id: number]: number }
     productsObject?: { [id: number]: Product }
+    CartItem?: any
 }
 
 const CartProductList = ({
     productsInCart,
     productsObject = getProductsObject(productsArray),
+    CartItem = CartProductListItem,
 }: Props) => {
-    console.log(productsObject)
-    // бачимо,що ключами  об'єкту є id, а значеннями- об'єкти product
-    console.log(productsInCart)
-    // бачимо, що ключами об'єкту є id, а значеннями- кількість вибраного товару
     return (
-        <div>
+        <>
             {Object.keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productsObject[+productId].title}:
-                    {productsInCart[+productId]}
-                </div>
-                // ключі об'єкту- title вибраного товару, а значення- його кількість
+                <CartItem
+                    key={productId}
+                    product={productsObject[+productId]}
+                    productCount={productsInCart[+productId]}
+                />
             ))}
-        </div>
+        </>
     )
 }
 
